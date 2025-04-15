8fe1eee26c98fe0365358f122dacf6fd54029e78
Grzegorz Smajdor <gs@deliverists.io>
2025-04-15 20:16:53 +0200

    small updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+starship init fish | source
+zoxide init fish | source
+alias cd=z # without causing an infinite loop.
+
 if status is-interactive
     fish_add_path /opt/homebrew/bin
     fish_add_path /opt/homebrew/sbin
@@ -11,3 +15,10 @@ end
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 set --export PATH ~/bin $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
+
+# Added by Windsurf
+fish_add_path /Users/sfistak/.codeium/windsurf/bin
