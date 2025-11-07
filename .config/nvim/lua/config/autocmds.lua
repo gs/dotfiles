b6c4af61b468f8a0a2a067fe12c42d00f50a0ce8
gs <gs@deliverists.io>
2025-11-07 11:55:35 +0100

    properly recognize yml file

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Ensure .yml files are recognized as yaml
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.yml",
+  callback = function()
+    vim.bo.filetype = "yaml"
+  end,
+})
